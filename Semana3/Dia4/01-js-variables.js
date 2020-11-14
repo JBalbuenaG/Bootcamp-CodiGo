596bf5149492016da8c2f7229f3e2ceb36b10a8d
Jose Balbuena <jose.balbuena@pucp.pe>
2020-11-13 23:10:28 -0500

    Semana3-Dia4
    Mediaquery e intro to JavaScript

---
+++ Semana3/Dia4/01-js-variables.js
@@ -0,0 +1,22 @@
+//Variables en JavaScript
+
+//let sirve para declarar una variable, tambien existen var y const
+let edad = 26; //variable del tipo number
+let sueldo = 950.5;
+
+//Variable del tipo cadena de texto
+let nombre = "José Balbuena";
+let mensaje = "I know you are there";
+
+//Variable booleana
+let casado = false;
+let cazado= true;
+
+//Variable del tipo arreglo
+let comidas = ["chaufa", "lentejas", "arroz con pollo"];
+
+//console.log()
+//Imprimir en la consola del navegador
+console.log("algo como esto");
+console.log(cazado);
+console.log("casado "+casado);
